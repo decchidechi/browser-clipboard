2e7002665be7a4124c36ef02a1472d2df2ff7f00
decchidechi <rkykvil@gmail.com>
2025-05-18 01:17:54 +0900

    chore: 各処理のコメント追加

--- src/FileReaderComponent.tsx
+++ src/FileReaderComponent.tsx
@@ -1,10 +1,32 @@
+/**
+ * FileReaderComponent
+ *
+ */
+
 import React, { useState, useCallback } from "react";
 import CopyToClipboardList from "./CopyToClipboardList";
 import styles from "./FileReaderComponent.module.css"; // CSS Modules をインポート
 
+/**
+ * @returns {JSX.Element} - テキストファイルを読み込み、内容をコピー可能なリストとして表示する React コンポーネント。
+ */
 const FileReaderComponent: React.FC = () => {
+  /**
+   * 読み込んだテキストファイルの行を格納するステート。
+   * @type {string[]}
+   */
   const [lines, setLines] = useState<string[]>([]);
+  /**
+   * 選択されたファイルのエンコーディングを格納するステート。
+   * @type {string}
+   */
 
+  /**
+   * ファイルが選択された際のイベントハンドラー。
+   * 選択されたテキストファイルの内容を読み込み、行ごとに `lines` ステートに格納する。
+   *
+   * @param {React.ChangeEvent<HTMLInputElement>} event - ファイル入力要素の change イベント。
+   */
   const handleFileChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       const file = event.target.files?.[0];
